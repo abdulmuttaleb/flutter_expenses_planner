6c85ff6e41c8079522f3c20e07c9e036f78197b9
AbdulMuttaleb <a.m.abdelmottaleb@gmail.com>
2020-07-11 03:50:43 +0200

    - updating flutter - conditional widget drawing depending on orientation

--- lib/main.dart
+++ lib/main.dart
@@ -117,6 +117,8 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
 
+    final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+
     final appBar = AppBar(
       title: Text('Personal Expenses'),
       actions: <Widget>[
@@ -127,6 +129,10 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
 
+    final txListWidget =  Container(
+        height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.7,
+        child: TransactionList(_userTransactions, _deleteTransaction));
+
     return Scaffold(
       appBar: appBar,
       body: SingleChildScrollView(
@@ -134,7 +140,7 @@ class _MyHomePageState extends State<MyHomePage> {
 //        mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Row(
+            if(isLandscape) Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text('Show Chart'),
@@ -148,14 +154,17 @@ class _MyHomePageState extends State<MyHomePage> {
                 )
               ],
             ),
-            _showChart ? Container(
+            if(!isLandscape)
+              Container(
+                  height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top)* 0.3,
+                  child: Chart(_recentTransactions)),
+              if(!isLandscape)
+                txListWidget,
+              if(isLandscape)_showChart ? Container(
                 height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top)* 0.7,
-                child: Chart(_recentTransactions)) :
-            Container(
-                height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.7,
-                child: TransactionList(_userTransactions, _deleteTransaction))
-          ],
-        ),
+                child: Chart(_recentTransactions))
+                : txListWidget
+        ]),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
